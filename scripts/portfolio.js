38b67bd60c5e63fdd7d24decde5a344f863c2ff7
ruffaultravenelg <gemino.ruffaultravenel@gmail.com>
2025-06-06 17:35:07 +0200

    Changed loading for portfolio

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,31 +1,30 @@
 const tabs = document.querySelectorAll('.tab-btn');
-const img = document.getElementById('content_image');
-const pdf = document.getElementById('content_pdf');
-const IMG_FODLER = "ressources/portfolio/"
+const contents = document.querySelectorAll('.content > *');
 
 
 tabs.forEach(tab => {
-
     tab.addEventListener('click', () => {
+       
         removeActiveClass();
         tab.classList.add('active');
-        const imgPath = tab.getAttribute('img');
 
-        if (imgPath === "pdf"){
-            pdf.hidden = false;
-            img.hidden = true;
-        } else {
-            pdf.hidden = true;
-            img.hidden = false;
-            img.src = IMG_FODLER + imgPath;
-        }
+        setContent(tab.getAttribute("attached"));
 
     });
-
 });
 
 function removeActiveClass() {
     tabs.forEach(tab => {
         tab.classList.remove('active');
     });
+}
+
+function setContent(key){
+    for (let content of contents) {
+        if (content.getAttribute("key") === key) {
+            content.hidden = false;
+        } else {
+            content.hidden = true;
+        }
+    }
 }
