c61ca121b9338d2ab4b24bbbcd1bb506a441ece9
ruffaultravenelg <gemino.ruffaultravenel@gmail.com>
2025-06-06 14:07:12 +0200

    Added competence

---
+++ scripts/portfolio.js
@@ -0,0 +1,31 @@
+const tabs = document.querySelectorAll('.tab-btn');
+const img = document.getElementById('content_image');
+const pdf = document.getElementById('content_pdf');
+const IMG_FODLER = "ressources/portfolio/"
+
+
+tabs.forEach(tab => {
+
+    tab.addEventListener('click', () => {
+        removeActiveClass();
+        tab.classList.add('active');
+        const imgPath = tab.getAttribute('img');
+
+        if (imgPath === "pdf"){
+            pdf.hidden = false;
+            img.hidden = true;
+        } else {
+            pdf.hidden = true;
+            img.hidden = false;
+            img.src = IMG_FODLER + imgSrc;
+        }
+
+    });
+
+});
+
+function removeActiveClass() {
+    tabs.forEach(tab => {
+        tab.classList.remove('active');
+    });
+}
